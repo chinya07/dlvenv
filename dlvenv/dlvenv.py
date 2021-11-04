9439bac2d46f03ca73356f81c2e9baa6ead97282
Chinmaya Kaundanya <38719024+chinya07@users.noreply.github.com>
2021-11-04 00:12:31 +0000

    Add files via upload

---
+++ dlvenv/dlvenv.py
@@ -0,0 +1,5 @@
+def info():
+    print("Welcome to Deep Learning Environment Module.")
+    print("-------------------------------")
+    print("Your Deep Learning Environment is Ready to use.")
+    print("-------------------------------")
